094b810861fdf5bf3c48d6aa20dc5438b22d8664
Ben Hanna <benpaulhanna@gmail.com>
2020-11-21 20:35:21 -0500

    Added description to product.

--- Data/Models/Product.cs
+++ Data/Models/Product.cs
@@ -6,9 +6,11 @@ namespace BlazorCommerce.Data.Models
     {
         public int Id { get; set; }
 
+        public int CategoryId { get; set; }
+
         public string Name { get; set; }
 
-        public int CategoryId { get; set; }
+        public string Description { get; set; }
 
         public IEnumerable<ProductVariant> ProductVariants { get; set; }
     }
